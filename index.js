c9496f47ad53071d829e07fca7bc301345e44f45
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-11-18 15:25:00 +0600

    Updated functioality added

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
 
     // middlewares 
     const verifyToken = (req, res, next) => {
-      console.log('inside verify token', req.headers);
+      // console.log('inside verify token', req.headers);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: 'unauthorized access' })
       }
@@ -131,6 +131,41 @@ async function run() {
     })
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray()
+      res.send(result)
+    })
+    app.get('/menu/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { $or:[{_id:new ObjectId(id)}, {_id : id}]};
+      // const query = {_id:new ObjectId(id) || id};
+      console.log(query);
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
+    app.patch('/menu/:id', async(req,res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id: id ||  new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          Image: item.Image
+        }
+      }
+      const result = await menuCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+    app.post('/menu',verifyToken, verifyAdmin, async(req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+    })
+    app.delete('/menu/:id',verifyToken,verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query);
       res.send(result)
     })
     app.get('/reviews', async (req, res) => {
